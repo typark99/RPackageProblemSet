02e5a51600a9e2647f77e33eb9b29386ca3d1a39
typark99 <typark99@gmail.com>
2014-03-13 16:37:27 -0500

    03132014_1
    Created an R package and wrote an R file for Regressions class

---
+++ regressionPack/R/Regressions.R
@@ -0,0 +1,54 @@
+#' A regression object 
+#' 
+#' Object of class \code{Regressions} are created by the \code{runReg} and \code{testReg} functions
+#'
+#' 
+#' An object of the class `Regressions' has the following slots:
+#' \itemize{
+#' \item \code{Y} A depedent variable
+#' \item \code{X} Covariates
+#' \item \code{coef} Coefficients from the output
+#' \item \code{R2} R^2 values from the output
+#' }
+#'
+#' @author Taeyong Park: \email{typark99@gmail.com}
+#' @aliases Regressions-class initialize,Regressions-method getRegressions,Regressions-method 
+#' @rdname Regressions
+#' @export
+setClass(Class="Regressions", 
+         slot = list(
+           Y = "matrix",
+           X = "matrix",
+           coef = "matrix",
+           R2 = "matrix"
+         ),
+         prototype = prototype(
+           Y = matrix(),
+           X = matrix(),
+           coef = matrix(),
+           R2 = matrix()
+         )
+)
+
+#' @export
+setMethod("initialize", "Regressions", 
+          function(.Object, ...){
+            value=callNextMethod()
+            return(value)
+          }
+) 
+
+#' @rdname Regressions
+#' @export 
+setGeneric("getRegressions",
+           function(object="Regressions")  {
+             standardGeneric("getRegressions")
+           }
+)
+
+#' @export
+setMethod("getRegressions", "Regressions",
+          function(object){ 
+            return(list(coef=object@coef,R2=object@R2)
+          }
+            )
